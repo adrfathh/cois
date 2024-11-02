63a60e0297f14115af42ecc41978bc391bb7a82a
dr vhi <a.fathirf3@gmail.com>
2024-11-02 13:47:31 +0700

    first commit

---
+++ sc3/script.js
@@ -0,0 +1,30 @@
+// Menampilkan Hello World
+console.log("Hello World");
+
+// Deklarasi variabel
+let nama = "Adrian Fathir";
+let umur = 18;
+console.log("Nama saya", nama, "dan saya berumur", umur, "tahun");
+console.log(`Nama saya ${nama} dan saya berumur ${umur} tahun`); // memanggil variabel dengan lebih mudah menggunakan (`)
+
+// Operator aritmatika
+let hasil1 = 5 + 3; // penjumlahan
+let hasil2 = 5 - 3; // pengurangan
+console.log(hasil1);
+console.log(hasil2);
+
+// Latihan
+console.log('- - - - - Latihan - - - - -')
+let name = 'Nathan';
+let age = 18;
+let hobby = 'belajar';
+console.log(`Alloo saya ${name}, umur saya ${age} dan hobi saya adalah ${hobby}`);
+
+let tambah = 15 + 3;
+let kurang = 15 - 3;
+let kali = 15 * 3;
+let bagi = 15 / 3;
+console.log(`hasil dari 15 + 3 adalah ${tambah}`);
+console.log(`hasil dari 15 - 3 adalah ${kurang}`);
+console.log(`hasil dari 15 x 3 adalah ${kali}`);
+console.log(`hasil dari 15 / 3 adalah ${bagi}`);
